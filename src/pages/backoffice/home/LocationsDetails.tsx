f3b267de0d7edb1ef1d1e7f2624225471985a4b0
Romera Jorge <romera.gomez.jorge@gmail.com>
2023-12-12 10:12:28 -0300

    Locations Detail: add a loader

--- src/pages/backoffice/home/LocationsDetails.tsx
+++ src/pages/backoffice/home/LocationsDetails.tsx
@@ -1,54 +1,68 @@
-import {TabContext, TabList, TabPanel} from '@mui/lab';
-import {Box, Card, CardContent, CardHeader, Tab} from '@mui/material';
-import {useSnackbar} from 'notistack';
-import {useState} from 'react';
-import {Layout as BackofficeLayout} from '../../../layouts/backoffice/Layout';
-import {useLocations} from '../location/locations.hooks';
-import {ContactUsTab} from './Tab/ContactUsTab';
-import {BasicInforamtionTab} from './Tab/BasicInforamtionTab';
-import { AddressTab } from './Tab/AddressTab';
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  CircularProgress,
+  Tab,
+} from "@mui/material";
+import { useSnackbar } from "notistack";
+import { useState } from "react";
+import { Layout as BackofficeLayout } from "../../../layouts/backoffice/Layout";
+import { useLocations } from "../location/locations.hooks";
+import { ContactUsTab } from "./Tab/ContactUsTab";
+import { BasicInforamtionTab } from "./Tab/BasicInforamtionTab";
+import { AddressTab } from "./Tab/AddressTab";
 
 export const LocationsDetails = () => {
-  const {enqueueSnackbar} = useSnackbar();
-  const {data: locations, isLoading, error} = useLocations();
+  const { enqueueSnackbar } = useSnackbar();
+  const { data: locations, isLoading, error } = useLocations();
 
   if (error) {
-    enqueueSnackbar(error.message, {variant: 'error'});
+    enqueueSnackbar(error.message, { variant: "error" });
   }
 
-  const [value, setValue] = useState('1');
+  const [value, setValue] = useState("1");
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
     setValue(newValue);
   };
   return (
-    <BackofficeLayout menuTitleSelected="Home">
-      <Card>
-        <CardHeader title="Locations"/>
-        <CardContent>
-          <TabContext value={value}>
-            <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-              <TabList
-                onChange={handleChange}
-                aria-label="lab API tabs example"
-              >
-                <Tab label="Basic Information" value="1"/>
-                <Tab label="Address" value="2"/>
-                <Tab label="Contact Us" value="3"/>
-              </TabList>
-            </Box>
-            <TabPanel value="1">
-              <BasicInforamtionTab/>
-            </TabPanel>
-            <TabPanel value="2">
-              <AddressTab/>
-            </TabPanel>
-            <TabPanel value="3" sx={{display: 'flex', gap: 5}}>
-              <ContactUsTab/>
-            </TabPanel>
-          </TabContext>
-        </CardContent>
-      </Card>
+    <BackofficeLayout menuTitleSelected="Dashboard">
+      {isLoading ? (
+        <Box sx={{display:"flex",justifyContent:"center",alignItems:"center",height:'100vh'}}>
+        <CircularProgress size={50} />
+      </Box>
+      ) : (
+        
+        <Card>
+          <CardHeader title="Locations" />
+          <CardContent>
+            <TabContext value={value}>
+              <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+                <TabList
+                  onChange={handleChange}
+                  aria-label="lab API tabs example"
+                >
+                  <Tab label="Basic Information" value="1" />
+                  <Tab label="Address" value="2" />
+                  <Tab label="Contact Us" value="3" />
+                </TabList>
+              </Box>
+              <TabPanel value="1">
+                <BasicInforamtionTab />
+              </TabPanel>
+              <TabPanel value="2">
+                <AddressTab />
+              </TabPanel>
+              <TabPanel value="3" sx={{ display: "flex", gap: 5 }}>
+                <ContactUsTab />
+              </TabPanel>
+            </TabContext>
+          </CardContent>
+        </Card>
+      )}
     </BackofficeLayout>
   );
 };
